08087a7c7feab57a3e899f558ce9931b4bd445bf
LiuJ <461698053@qq.com>
2016-08-25 14:45:44 +0800

    Update Craw_Pic.py

--- Craw_Pic.py
+++ Craw_Pic.py
@@ -1,13 +1,14 @@
 #coding=utf-8
 '''
-------------整站抓取图片,使用gevent+requests异步下载方式------------------
+********整站抓取图片,使用gevent+requests异步下载方式********
 *1.lxml包xpath工具获取url,其中xpath从chrome开发者工具获取
 *2.使requests下载
 *3.正则爬取url所有图片
-*4.异步IO处理图片下载任务
+*4.协程处理图片下载任务
+*5.tqdm进度显示
 '''
-import re
-import requests
+import re,requests
+from tqdm import *
 import os,time
 from lxml import etree
 from gevent import monkey;monkey.patch_all()
@@ -17,18 +18,12 @@ ISOTIMEFORMAT='%Y-%m-%d %X'
 #--------创建路径,储存图片------------
 BASEPATH = os.getcwd()+'/49vvpic/'
 
-
-def print_node(node): #打印Element元素
-    print "==============================================" 
-    print "node.attrib:%s" % node.attrib 
-    print "node.attrib['href']:%s" % node.attrib['href'] 
-
 def get_urls(root_url,numth_url):
     urls = set()
     res = requests.get(numth_url)
     selector = etree.HTML(res.content)
     #------取中文标题------"
-#     //*[@id="gotop"]/div[2]/div/div/div[3]/div/ul/li[6]/a/text()
+    #//*[@id="gotop"]/div[2]/div/div/div[3]/div/ul/li[6]/a/text()
 #     pic_name = selector.xpath("//*[@id='gotop']/div[2]/div/div/div[3]/div/ul/li/a/text()")
 #     for i in pic_name:  #对应的url中文标题
 #         print i
@@ -43,8 +38,8 @@ def get_pic_url(url):   #获取指定url下的所有图片url
     res = requests.get(url)
     #图片所在标签: <a href=""><img src="" alt=""/></a></P>    
     pic_urls = re.findall(r'<a href=".+?"><img src="(.+?)" alt=".+?"/></a></P>', res.content, re.S) #正则匹配
-    print '图片url:',pic_urls,'\n图片数量:',len(pic_urls)
-    return pic_urls if len(pic_urls)<=10 else pic_urls[:10]    #只取前10个,因为很多重复的图片
+    print '\n图片数量:',len(pic_urls)
+    return pic_urls if len(pic_urls)<=10 else pic_urls[:20]    #每个网页最多下20张,因为很多重复的图片
         
 def download(url):#图片下载
     url = re.sub(r'%2[eE]','.',url,re.IGNORECASE)   #如果.被编码为%2E则改回.
@@ -91,10 +86,10 @@ if __name__=='__main__':
     if not os.path.isdir(BASEPATH):
         os.mkdir(BASEPATH)    
     #开始下载
-    for url in urls:
+    for url in tqdm(urls):  #tqdm封装迭代器,可以显示进度
         pic_urls = get_pic_url(url)         #正则爬取图片url
         gevent.joinall([gevent.spawn(download,pic_url) for pic_url in pic_urls])
-    print '下载完成!!'
+    print '\n下载完成!!'
     
         
     
