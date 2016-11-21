65da75f6128ee5a6900536c47bd84c80fe6207d1
kushao1267 <jianliu001922@sohu-inc.com>
2016-11-21 20:11:38 +0800

    craw_pic的python3版本亲测可用，加入socks5代理,加入requirements

--- craw_pic.py
+++ craw_pic.py
@@ -1,16 +1,20 @@
 # coding=utf-8
 '''
 ********  整站抓取图片,使用gevent+requests异步下载方式  ********
-	*1.lxml包xpath工具获取url,其中xpath从chrome开发者工具获取
-	*2.使requests下载
-	*3.正则爬取url所有图片
-	*4.协程处理图片下载任务
-	*5.tqdm显示进度
-	*6.自动打开文件夹窗口展示图片
+		*1.lxml包xpath工具获取url,其中xpath从chrome开发者工具获取
+		*2.使requests下载
+		*3.正则爬取url所有图片
+		*4.协程处理图片下载任务
+		*5.加入代理proxies
+		*6.自动打开文件夹窗口展示图片
+		requirements:
+			requests (2.11.1)
+			PySocks (1.5.7)
+			lxml (3.6.4)
+			gevent (1.1.2)
 '''
 import re
 import requests
-import tqdm
 import os
 import time
 from lxml import etree
@@ -21,6 +25,11 @@ monkey.patch_all()
 # --------创建路径,储存图片------------
 BASEPATH = os.getcwd() + '/49vvpic/'
 ORIGIN_URL = 'http://www.de4f.com/404.html?/'
+# 使用socks5代理，可用其他vpn.先设置本地1080端口翻墙，然后设置requests的代理为该端口
+PROXIES = {
+    'http': 'socks5://127.0.0.1:1080',
+    'https': 'socks5://127.0.0.1:1080'
+}
 
 
 def get_urls(root_url, numth_url):
@@ -46,9 +55,13 @@ def get_pic_url(url):  # 获取指定url下的所有图片url
     return pic_urls if len(pic_urls) <= 10 else pic_urls[:20]
 
 
-def download(title, url):  # 图片下载
+def download(title, url, proxy_on=False):  # 图片下载
+    if proxy_on:
+        proxies = PROXIES
+    else:
+        proxies = {}
     url = re.sub('%2[eE]', '.', url, re.IGNORECASE)  # 如果.被编码为%2E则改回.
-    res = requests.get(url)
+    res = requests.get(url, proxies=proxies)
     print('正在下载图片:' + url)
     name = re.split('\.|/+', url)
     filesavepath = title + name[-3] + str(time.time()) + '.' + name[-1]
@@ -61,7 +74,6 @@ def choose(root_url):
     areas = ['asia', 'oumei', 'zipai', 'meitui', 'cartoon']
     while True:
         area = input('看什么区?请输入拼音:')
-        print(area)
         if area in areas:
             break
     number = input('爬取第几页?:')
@@ -70,14 +82,14 @@ def choose(root_url):
     return root_url + 'AAtupian/AAtb/' + area + '/index' + number + '.html'
 
 
-def do_work(BASEPATH, urls):
-    for pic_name, url in tqdm(urls):  # tqdm封装迭代器,显示进度
+def do_work(BASEPATH, urls, proxy_on=False):
+    for pic_name, url in urls:  # tqdm封装迭代器,显示进度
         title = BASEPATH + pic_name + '/'  # 根据标题,创建子目录,易于区分图集
         print('\n开始下载:{0}'.format(pic_name))
         pic_urls = get_pic_url(url)  # 正则爬取图片url
         if not os.path.isdir(title) and len(pic_urls) > 0:
             os.mkdir(title)
-        gevent.joinall([gevent.spawn(download, title, pic_url)
+        gevent.joinall([gevent.spawn(download, title, pic_url, proxy_on)
                         for pic_url in pic_urls])  # gevent并发下载
     print('\n下载完成!!')
 
@@ -108,8 +120,8 @@ def test():
     if not os.path.isdir(BASEPATH):
         os.mkdir(BASEPATH)
         os.system('nautilus ' + BASEPATH)  # 打开文件夹图像窗口
-    # 开始下载
-    do_work(BASEPATH, urls)
+    # 开始下载,开启代理
+    do_work(BASEPATH, urls, True)
 
 
 if __name__ == '__main__':
