64fd1a12c70476cb7d7b94a345229cad934738bb
kushao1267 <jianliu001922@sohu-inc.com>
2016-12-22 11:43:28 +0800

    review and add ss script

---
+++ shadowsocks/ssconfig.py
@@ -0,0 +1,89 @@
+# coding=utf-8
+import requests
+from lxml import etree
+from subprocess import Popen
+import json
+import sys
+
+vpn_url = 'http://www.ishadowsocks.org/'
+
+# config
+config = {
+    "server": 'server',
+    "server_port": 'server_port',
+    "local_address": '127.0.0.1',
+    "local_port": '1080',
+    "password": 'password',
+    "timeout": 500,
+    "method": 'method',
+    "fast_open": False
+}
+
+point = {
+    '0': {  # ss
+        'server': "",# server host
+        'server_port': ,# server port
+        'password': "", # passwd
+        'method': "rc4-md5" # method
+    },
+    '1': {  # iss A
+        'server': '//*[@id="free"]/div/div[2]/div[1]/h4[1]',
+        'server_port': '//*[@id="free"]/div/div[2]/div[1]/h4[2]',
+        'password': '//*[@id="free"]/div/div[2]/div[1]/h4[3]',
+        'method': '//*[@id="free"]/div/div[2]/div[1]/h4[4]'
+    },
+    '2': {  # iss B
+        'server': '//*[@id="free"]/div/div[2]/div[2]/h4[1]',
+        'server_port': '//*[@id="free"]/div/div[2]/div[2]/h4[2]',
+        'password': '//*[@id="free"]/div/div[2]/div[2]/h4[3]',
+        'method': '//*[@id="free"]/div/div[2]/div[2]/h4[4]'
+    },
+    '3': {  # iss C
+        'server': '//*[@id="free"]/div/div[2]/div[3]/h4[1]',
+        'server_port': '//*[@id="free"]/div/div[2]/div[3]/h4[2]',
+        'password': '//*[@id="free"]/div/div[2]/div[3]/h4[3]',
+        'method': '//*[@id="free"]/div/div[2]/div[3]/h4[4]'
+    }
+}
+
+
+def select_point(iss, res):
+    server = etree.HTML(res.content).xpath(
+        point[iss]['server'])[0].text.split(':')[1] \
+        if iss not in ['0'] else point[iss]['server']
+    server_port = etree.HTML(res.content).xpath(
+        point[iss]['server_port'])[0].text.split(':')[1] \
+        if iss not in ['0'] else point[iss]['server_port']
+    password = etree.HTML(res.content).xpath(
+        point[iss]['password'])[0].text.split(':')[1] \
+        if iss not in ['0'] else point[iss]['password']
+    method = etree.HTML(res.content).xpath(
+        point[iss]['method'])[0].text.split(':')[1] \
+        if iss not in ['0'] else point[iss]['method']
+    return (server, server_port, password, method)
+
+
+def main():
+    '''
+            免费翻墙
+    '''
+    iss = sys.argv[1]
+    file = sys.argv[2]
+    res = requests.get(vpn_url)
+    # 选择节点,配置ss服务器
+    config['server'], config['server_port'], config[
+        'password'], config['method'] = select_point(iss, res)
+    # 改变配置
+    try:
+        with open(file, 'w') as f:
+            json.dump(config, f)
+            # 运行配置脚本
+            Popen('sslocal -c ' + file, shell=True)
+    except Exception:
+        print('打开文件失败！')
+    finally:
+        exit(0)
+
+
+if __name__ == '__main__':
+    main()
---
+++ shadowsocks/ssstart.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+import requests
+from subprocess import Popen
+import os
+
+PROXY = {'http': "socks5://127.0.0.1:1080"}  # 配置本地代理端口
+TIMEOUT = 4
+URL_CHECK = 'http://www.google.com'  # 用于检测ss是否连接成功
+CONFIG_NAME = 'ss_free'  # ss的配置文件名,可自定义
+
+
+def main():
+    '''
+        自动检测是否翻墙，清除后台，重新选择节点。
+    '''
+    print('-' * 43)
+    try:
+        gen_config(CONFIG_NAME)
+        key = check()
+        if key in ['Y', 'y']:
+            switch()
+    except:
+        print('未连接VPN...')
+        switch()
+    finally:
+        exit(0)
+        print('-' * 43)
+
+
+def gen_config(file_name):
+    '''
+        生成ss的config文件,并修改权限
+    '''
+    try:
+        global FILE
+        FILE = '/etc/' + file_name + '.json'
+        cmd = 'sudo chmod 777 ' + \
+            FILE if os.path.exists(FILE) else 'sudo touch ' + \
+            FILE + ' ; sudo chmod 777 ' + FILE
+        Popen(cmd, shell=True)
+    except:
+        print('请使用超级权限!')
+        exit(0)
+
+
+def check():
+    '''
+        检查节点是否可用
+    '''
+    proxies = PROXY  # 安装pysocks使requests能够代理
+    res = requests.get(URL_CHECK,
+                       proxies=proxies, timeout=TIMEOUT)
+    print('VPN已连接 :{} !'.format(res.status_code))
+    key = input('是否切换VPN？ y/Y')
+    return key
+
+
+def switch():
+    '''
+        重启翻墙程序.
+        防止上次后台运行时的进程占用端口.
+    '''
+    kill_process('sslocal')
+    iss = input('选择节点?  (0,1,2,3)?')
+    if iss not in ['0', '1', '2', '3']:
+        iss = '0'
+    task = 'nohup python3 ' + os.getcwd() + '/' + '{0} {1} {2} & '.format(
+        'internet.py', iss, FILE)
+    print(task)
+    Popen(task, shell=True)
+    print('连接成功...')
+
+
+def kill_process(pid_name):
+    '''
+        kill指定名称的进程
+    '''
+    Popen("pkill -f {0}".format(pid_name), shell=True)
+    print('清除进程...')
+
+
+if __name__ == '__main__':
+    main()
