d591f401d837512589d5297f3cb7e3bec77b6ae4
Krusjra <461698053@qq.com>
2018-04-17 23:33:57 +0800

    重新加入进度条
    新url，可抓取

--- craw_pic.py
+++ craw_pic.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-'''
+"""
 ********  整站抓取图片,使用gevent+requests异步下载方式  ********
 		*1.lxml包xpath工具获取url,其中xpath从chrome开发者工具获取
 		*2.使requests下载
@@ -7,30 +7,31 @@
 		*4.协程处理图片下载任务
 		*5.加入代理proxies
 		*6.自动打开文件夹窗口展示图片
-		
+
 	requirements:
 		- requests (2.11.1)
-		- PySocks (1.5.7) - socks代理      
+		- PySocks (1.5.7) - socks代理
 		- lxml (3.6.4)
 		- gevent (1.1.2)
-'''
+        	- tqdm (4.23.0)
+"""
 import re
-import requests
 import os
 import time
+import requests
+from tqdm import tqdm
 from lxml import etree
 import gevent
 from gevent import monkey
-monkey.patch_all()
+
 
 # --------创建路径,储存图片------------
 BASEPATH = os.getcwd() + '/49vvpic/'
-ORIGIN_URL = 'http://www.de4f.com/404.html?/'
+# http://qwx2.com/index.html # curl访问，用于获取ORIGIN_URL
+ORIGIN_URL = 'http://www.gf3s.com/404.html?/'
 # 使用socks5代理，可用其他vpn.先设置本地1080端口翻墙，然后设置requests的代理为该端口
-PROXIES = {
-    'http': 'socks5://127.0.0.1:1080',
-    'https': 'socks5://127.0.0.1:1080'
-}
+PROXIES = dict(http='socks5://127.0.0.1:1080', https='socks5://127.0.0.1:1080')
+TIMEOUT = 10  # 单位:秒
 
 
 def get_urls(root_url, numth_url):
@@ -51,7 +52,6 @@ def get_pic_url(url):  # 获取指定url下的所有图片url
     pic_urls = re.findall(
         '<a href=".*?"><img  src="(.*?)" alt=".*?" /></a></P>',
         res.text, re.S)  # 正则匹配
-    print('\n图片数量:', len(pic_urls))
     # 每个网页最多下20张,因为很多重复的图片
     return pic_urls if len(pic_urls) <= 10 else pic_urls[:20]
 
@@ -62,7 +62,7 @@ def download(title, url, proxy_on=False):  # 图片下载
     else:
         proxies = {}
     url = re.sub('%2[eE]', '.', url, re.IGNORECASE)  # 如果.被编码为%2E则改回.
-    res = requests.get(url, proxies=proxies)
+    res = requests.get(url, proxies=proxies, timeout=TIMEOUT)
     print('正在下载图片:' + url)
     name = re.split('\.|/+', url)
     filesavepath = title + name[-3] + str(time.time()) + '.' + name[-1]
@@ -84,10 +84,16 @@ def choose(root_url):
 
 
 def do_work(BASEPATH, urls, proxy_on=False):
-    for pic_name, url in urls:  # tqdm封装迭代器,显示进度
+    urls_dict = dict(urls)
+    for pic_name in tqdm(urls_dict):  # tqdm封装迭代器,显示进度
         title = BASEPATH + pic_name + '/'  # 根据标题,创建子目录,易于区分图集
-        print('\n开始下载:{0}'.format(pic_name))
-        pic_urls = get_pic_url(url)  # 正则爬取图片url
+        print("\n" + "-----------" * 6)
+        try:
+            print('\n开始下载:{0}, 图片数:{1}张'.format(
+                pic_name, pic_name.split("P)")[0].split("(")[-1]))
+        except Exception:
+            pass
+        pic_urls = get_pic_url(urls_dict[pic_name])  # 正则爬取图片url
         if not os.path.isdir(title) and len(pic_urls) > 0:
             os.mkdir(title)
         gevent.joinall([gevent.spawn(download, title, pic_url, proxy_on)
@@ -95,7 +101,7 @@ def do_work(BASEPATH, urls, proxy_on=False):
     print('\n下载完成!!')
 
 
-def test():
+def craw():
     try:
         # 从防黑页面获取root_url,防止网页挂掉
         r = requests.get(ORIGIN_URL, allow_redirects=True)
@@ -110,7 +116,7 @@ def test():
         print('网页已挂，请更改ORIGIN_URL。')
         exit(0)     # 退出
     numth_url = choose(root_url[0])
-    print(numth_url)
+    print("正在爬取:{}".format(numth_url))
     # 解决指定页有时返回0个url的情况
     while True:
         (numofurl, urls) = get_urls(root_url[0], numth_url)
@@ -125,4 +131,5 @@ def test():
 
 
 if __name__ == '__main__':
-    test()
+    monkey.patch_all()
+    craw()
